074b034e446f91201936947e1a7c7bdfef876eb7
John Njuki <njukijohn2000@gmail.com>
2022-09-07 21:50:51 +0300

    implement the app's drawer header

--- app/src/main/java/com/example/myapplication/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/example/myapplication/appdrawer/AppDrawer.kt
@@ -36,21 +36,24 @@ package com.example.myapplication.appdrawer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.myapplication.R
 import com.example.myapplication.theme.JetRedditThemeSettings
 
@@ -59,20 +62,20 @@ import com.example.myapplication.theme.JetRedditThemeSettings
  */
 @Composable
 fun AppDrawer(
-  modifier: Modifier = Modifier,
-  closeDrawerAction: () -> Unit
+    modifier: Modifier = Modifier,
+    closeDrawerAction: () -> Unit
 ) {
-  Column(
-    modifier = modifier
-      .fillMaxSize()
-      .background(color = MaterialTheme.colors.surface)
-  ) {
-    AppDrawerHeader()
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        AppDrawerHeader()
 
-    AppDrawerBody(closeDrawerAction)
+        AppDrawerBody(closeDrawerAction)
 
-    AppDrawerFooter(modifier)
-  }
+        AppDrawerFooter(modifier)
+    }
 }
 
 /**
@@ -80,22 +83,125 @@ fun AppDrawer(
  */
 @Composable
 private fun AppDrawerHeader() {
-  //TODO add your code here
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            imageVector = Icons.Filled.AccountCircle,
+            colorFilter = ColorFilter.tint(Color.LightGray),
+            modifier = Modifier
+                .padding(16.dp)
+                .size(50.dp),
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center,
+            contentDescription = stringResource(id = R.string.account)
+        )
+        Text(
+            text = stringResource(id = R.string.default_username),
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        ProfileInfo()
+    }
+
+    Divider(
+        color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+    )
 }
 
 @Composable
-fun ProfileInfo() {
-  //TODO add your code here
+fun ProfileInfo(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        val (karmaItem, divider, ageItem) = createRefs()
+        val colors = MaterialTheme.colors
+
+        ProfileInfoItem(
+            iconAsset = Icons.Filled.Star,
+            amountResourceId = R.string.default_karma_amount,
+            textResourceId = R.string.karma,
+            modifier = modifier.constrainAs(karmaItem) {
+                centerVerticallyTo(parent)
+                start.linkTo(parent.start)
+            }
+        )
+
+        Divider(
+            modifier = modifier
+                .width(1.dp)
+                .constrainAs(divider) {
+                    centerVerticallyTo(karmaItem)
+                    centerHorizontallyTo(parent)
+                    height = Dimension.fillToConstraints
+                },
+            color = colors.onSurface.copy(alpha = .2f)
+        )
+
+        ProfileInfoItem(
+            iconAsset = Icons.Filled.ShoppingCart,
+            amountResourceId = R.string.default_reddit_age_amount,
+            textResourceId = R.string.reddit_age,
+            modifier = modifier.constrainAs(ageItem) {
+                start.linkTo(divider.end)
+                centerVerticallyTo(parent)
+            }
+        )
+    }
 }
 
 @Composable
 private fun ProfileInfoItem(
-  iconAsset: ImageVector,
-  amountResourceId: Int,
-  textResourceId: Int,
-  modifier: Modifier
+    iconAsset: ImageVector,
+    amountResourceId: Int,
+    textResourceId: Int,
+    modifier: Modifier
 ) {
-  //TODO add your code here
+    val colors = MaterialTheme.colors
+    ConstraintLayout(modifier = modifier) {
+        val (iconRef, amountRef, titleRef) = createRefs()
+        val itemModifier =
+            Modifier // Good practice to differentiate between parent & item modifiers
+        Icon(
+            contentDescription = stringResource(id = textResourceId),
+            imageVector = iconAsset,
+            tint = Color.Blue,
+            modifier = itemModifier
+                .constrainAs(iconRef) {
+                    centerVerticallyTo(parent)
+                    start.linkTo(parent.start)
+                }
+                .padding(start = 16.dp)
+        )
+        Text(
+            text = stringResource(id = amountResourceId),
+            color = colors.primaryVariant,
+            fontSize = 10.sp,
+            modifier = itemModifier
+                .padding(start = 8.dp)
+                .constrainAs(amountRef) {
+                    top.linkTo(iconRef.top)
+                    start.linkTo(iconRef.end)
+                    bottom.linkTo(iconRef.top)
+                }
+        )
+        Text(
+            text = stringResource(id = textResourceId),
+            color = Color.Gray,
+            fontSize = 10.sp,
+            modifier = itemModifier
+                .padding(start = 8.dp)
+                .constrainAs(titleRef) {
+                    top.linkTo(amountRef.bottom)
+                    start.linkTo(iconRef.end)
+                    bottom.linkTo(iconRef.bottom)
+                }
+        )
+    }
 }
 
 /**
@@ -105,7 +211,7 @@ private fun ProfileInfoItem(
  */
 @Composable
 private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
-  //TODO add your code here
+    //TODO add your code here
 }
 
 /**
@@ -113,46 +219,46 @@ private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
  */
 @Composable
 private fun ScreenNavigationButton(
-  icon: ImageVector,
-  label: String,
-  onClickAction: () -> Unit,
-  modifier: Modifier = Modifier
+    icon: ImageVector,
+    label: String,
+    onClickAction: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-  val colors = MaterialTheme.colors
+    val colors = MaterialTheme.colors
 
-  val surfaceModifier = modifier
-    .padding(start = 8.dp, top = 8.dp, end = 8.dp)
-    .fillMaxWidth()
+    val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
 
-  Surface(
-    modifier = surfaceModifier,
-    color = colors.surface,
-    shape = MaterialTheme.shapes.small
-  ) {
-    TextButton(
-      onClick = onClickAction,
-      modifier = Modifier.fillMaxWidth()
+    Surface(
+        modifier = surfaceModifier,
+        color = colors.surface,
+        shape = MaterialTheme.shapes.small
     ) {
-      Row(
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-      ) {
-        Image(
-          imageVector = icon,
-          colorFilter = ColorFilter.tint(Color.Gray),
-          contentDescription = label
-        )
-        Spacer(Modifier.width(16.dp))
-        Text(
-          fontSize = 10.sp,
-          text = label,
-          style = MaterialTheme.typography.body2,
-          color = colors.primaryVariant
-        )
-      }
+        TextButton(
+            onClick = onClickAction,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    imageVector = icon,
+                    colorFilter = ColorFilter.tint(Color.Gray),
+                    contentDescription = label
+                )
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    fontSize = 10.sp,
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = colors.primaryVariant
+                )
+            }
+        }
     }
-  }
 }
 
 /**
@@ -160,20 +266,20 @@ private fun ScreenNavigationButton(
  */
 @Composable
 private fun AppDrawerFooter(modifier: Modifier = Modifier) {
-  //TODO add your code here
+    //TODO add your code here
 }
 
 private fun changeTheme() {
-  JetRedditThemeSettings.isInDarkTheme.value = JetRedditThemeSettings.isInDarkTheme.value.not()
+    JetRedditThemeSettings.isInDarkTheme.value = JetRedditThemeSettings.isInDarkTheme.value.not()
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ProfileInfoItemPreview() {
-  ProfileInfoItem(
-    Icons.Filled.ShoppingCart,
-    R.string.default_reddit_age_amount,
-    R.string.reddit_age,
-    Modifier
-  )
+    ProfileInfoItem(
+        Icons.Filled.ShoppingCart,
+        R.string.default_reddit_age_amount,
+        R.string.reddit_age,
+        Modifier
+    )
 }
